15556195e173436f98eee6d9d986f890299e7cc4
yankit293 <yankit293@gmail.com>
2022-07-03 17:44:20 +0530

    Sorting: Update variable names

--- Sorting/MergeSort.c
+++ Sorting/MergeSort.c
@@ -7,23 +7,23 @@ void swap(int *a, int *b) {
   *b = tmp;
   
 }
-void merge(int arr[], int l, int m, int r)
+void merge(int arr[], int low, int m, int high)
 {
     int i, j, k;
-    int n1 = m - l + 1;
-    int n2 = r - m;
+    int n1 = m - low + 1;
+    int n2 = high - m;
  
     int L[n1], R[n2];
  
     for (i = 0; i < n1; i++)
-        L[i] = arr[l + i];
+        L[i] = arr[low + i];
     for (j = 0; j < n2; j++)
         R[j] = arr[m + 1 + j];
  
 
     i = 0; 
     j = 0; 
-    k = l; 
+    k = low; 
     while (i < n1 && j < n2) {
         if (L[i] <= R[j]) {
             arr[k] = L[i];
